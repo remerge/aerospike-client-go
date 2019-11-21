c71c2d15bdecd110ef9d4c00a5fa2f233439e5e4
Martin Karlsch <martin@karlsch.com>
2019-11-21 17:04:34 +0100

    add dedicated error if connection pool is empty and connection queue limit is reached

--- types/error.go
+++ types/error.go
@@ -75,6 +75,7 @@ var ErrServerNotAvailable = NewAerospikeError(SERVER_NOT_AVAILABLE)
 var ErrKeyNotFound = NewAerospikeError(KEY_NOT_FOUND_ERROR)
 var ErrRecordsetClosed = NewAerospikeError(RECORDSET_CLOSED)
 var ErrConnectionPoolEmpty = NewAerospikeError(NO_AVAILABLE_CONNECTIONS_TO_NODE, "Connection pool is empty. This happens when either all connection are in-use already, or no connections were available")
+var ErrConnectionPoolEmptyAndAllConnectionsInUse = NewAerospikeError(NO_AVAILABLE_CONNECTIONS_TO_NODE, "Connection pool is empty and all connections are in use. This happens when all connection are in-use already")
 var ErrTooManyConnectionsForNode = NewAerospikeError(NO_AVAILABLE_CONNECTIONS_TO_NODE, "Connection limit reached for this node. This value is controled via ClientPolicy.LimitConnectionsToQueueSize")
 var ErrTooManyOpeningConnections = NewAerospikeError(NO_AVAILABLE_CONNECTIONS_TO_NODE, "Too many connections are trying to open at once. This value is controled via ClientPolicy.OpeningConnectionThreshold")
 var ErrTimeout = NewAerospikeError(TIMEOUT, "command execution timed out on client: See `Policy.Timeout`")
@@ -82,3 +83,4 @@ var ErrUDFBadResponse = NewAerospikeError(UDF_BAD_RESPONSE, "Invalid UDF return 
 var ErrNoOperationsSpecified = NewAerospikeError(INVALID_COMMAND, "No operations were passed to QueryExecute")
 var ErrNoBinNamesAlloedInQueryExecute = NewAerospikeError(INVALID_COMMAND, "Statement.BinNames must be empty for QueryExecute")
 var ErrFilteredOut = NewAerospikeError(FILTERED_OUT)
+
