77e7ffb846f482324475ee0a9c0dbb299d13ebad
Georgy Buranov <gburanov@gmail.com>
2025-10-29 15:05:22 +0100

    Add lazy commad

--- read_command.go
+++ read_command.go
@@ -20,6 +20,7 @@ import (
 
 	"github.com/aerospike/aerospike-client-go/logger"
 	"github.com/aerospike/aerospike-client-go/types"
+	ParticleType "github.com/aerospike/aerospike-client-go/types/particle_type"
 
 	Buffer "github.com/aerospike/aerospike-client-go/utils/buffer"
 )
@@ -34,6 +35,7 @@ type readCommand struct {
 	// pointer to the object that's going to be unmarshalled
 	object *reflect.Value
 
+	lazy            bool
 	replicaSequence int
 }
 
@@ -221,7 +223,15 @@ func (cmd *readCommand) parseRecord(
 		receiveOffset += 4 + 4 + nameSize
 
 		particleBytesSize := opSize - (4 + nameSize)
-		value, _ := bytesToParticle(particleType, cmd.dataBuffer, receiveOffset, particleBytesSize)
+		var value interface{}
+		if cmd.lazy && particleType == ParticleType.MAP {
+			// we need to make a copy as the buffer is reused
+			b := make([]byte, particleBytesSize)
+			copy(b, cmd.dataBuffer[receiveOffset:receiveOffset+particleBytesSize])
+			value = newUnpacker(b, 0, particleBytesSize)
+		} else {
+			value, _ = bytesToParticle(particleType, cmd.dataBuffer, receiveOffset, particleBytesSize)
+		}
 		receiveOffset += particleBytesSize
 
 		if bins == nil {
